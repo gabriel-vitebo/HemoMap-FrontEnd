4dee17a5f69c10a64e2baff61877b41d87a91297
gabriel.vitebo <gabriel.vitebo@redealumni.com>
2025-02-19 17:43:54 -0300

    chore: initial project structure

---
+++ src/components/footer/footer.schema.ts
@@ -0,0 +1,7 @@
+import { tv } from "tailwind-variants";
+
+export const schema = tv({
+  slots: {
+    base: 'w-full bg-yellow-600 text-2xl font-bold text-white p-4 flex items-center justify-center',
+  }
+}) 
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Header from '~/components/header/header.vue';
+import Body from '~/components/body/body.vue';
+import Footer from '~/components/footer/footer.vue';
+
+</script>
+
+<template>
+  <Header />
+  <Body>
+    <template v-slot:body>
+      <h1>Aqui vai todo o conteudo do Body</h1>
+    </template>
+  </Body>
+  <Footer />
+</template>
