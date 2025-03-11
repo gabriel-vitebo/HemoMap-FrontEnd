6d5aad1c88c328627c015c93d9f83cc12956411f
gabriel.vitebo <gabriel.vitebo@redealumni.com>
2025-03-11 17:02:15 -0300

    feat: add footer

--- src/components/footer/footer.schema.ts
+++ src/components/footer/footer.schema.ts
@@ -2,6 +2,6 @@ import { tv } from "tailwind-variants";
 
 export const schema = tv({
   slots: {
-    base: 'w-full bg-yellow-600 text-2xl font-bold text-white p-4 flex items-center justify-center',
+    base: 'w-full bg-cyan-600 text-2xl text-white p-4 flex items-start',
   }
 }) 
