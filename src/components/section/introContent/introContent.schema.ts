3aa1007ec66d4073f038fab7ca33647fa9c4489c
gabriel.vitebo <gabriel.vitebo@redealumni.com>
2025-04-02 17:52:03 -0300

    feat: create schema to styled

---
+++ src/components/section/introContent/introContent.schema.ts
@@ -0,0 +1,11 @@
+import { tv } from "tailwind-variants";
+
+export const schema = tv({
+  slots: {
+    base: 'flex flex-col w-full gap-8',
+    banner: 'relative w-full h-96 overflow-hidden',
+    bannerImage: 'size-full object-cover',
+    bannerContent: 'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white drop-shadow-[2px_2px_4px_rgba(0,0,0,0.5)]',
+    bannerContentTitle: 'text-4xl mb-4',
+  }
+})
