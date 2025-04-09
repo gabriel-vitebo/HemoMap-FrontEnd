1c067a3db38d06dd671d0d9689fbecee6c150658
gabriel.vitebo <gabriel.vitebo@redealumni.com>
2025-04-09 17:39:44 -0300

    feat: add aboutUs component

---
+++ src/components/section/aboutUs/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { schema } from './aboutUs.schema';
+
+const ui = computed(() => {
+  return schema();
+})
+</script>
+
+<template>
+  <div :class="ui.base()">
+    <h2>Sobre nós</h2>
+    <div :class="ui.content()">
+      <p :class="ui.text()">
+        Somos estudantes de Tecnologia da Informação na ETEP e, como parte do nosso 
+        projeto de semestre, decidimos usar nossos conhecimentos para impactar
+        positivamente a sociedade. Sabemos que muitas pessoas têm vontade 
+        de doar sangue, mas não sabem por onde começar — 
+        foi pensando nisso que surgiu a nossa ideia.
+      </p>
+      <p :class="ui.text()">
+        Nosso objetivo é facilitar o acesso à informação sobre doação de sangue 
+        e incentivar mais pessoas a participarem desse gesto tão importante. 
+        Acreditamos no poder da tecnologia como ferramenta de transformação 
+        social e estamos muito motivados em fazer a diferença com este projeto.
+      </p>
+    </div>
+  </div>
+</template>
--- src/components/section/introContent/index.vue
+++ src/components/section/introContent/index.vue
@@ -2,6 +2,13 @@
 import heartFallBack from '~/assets/fallback/heart.jpg' 
 import { schema } from './introContent.schema';
 
+const props = defineProps<{
+  options: 
+    {
+      title: string,
+    }[]
+}>();
+
 const ui = computed(() => {
   return schema();
 });
@@ -22,30 +29,12 @@ const ui = computed(() => {
       </div>
     </div>
 
-    <div class="flex flex-col gap-4 p-4">
-      <div class="w-full active">
-        <button class="w-full p-4 bg-red-600 border-none rounded-[8px] text-xl font-bold">Quero doar agora</button>
-      </div>
-      <div class="sidebar-option">
-        <button class="secondary-button">Como funciona</button>
-      </div>
-      <div class="sidebar-option">
-        <button class="secondary-button">Sobre nós</button>
-      </div>
+    <div 
+      v-for="(option, key) in props.options"
+      :key
+      class="flex flex-col gap-1 p-4"
+    >
+      <button :class="ui.button()">{{ option.title }}</button>
     </div>
   </div>
 </template>
-
-  <style scoped>
-  .secondary-button {
-    width: 100%;
-    padding: 1rem;
-    background-color: #f8f9fa;
-    color: #343a40;
-    border: 1px solid #dee2e6;
-    border-radius: 8px;
-    font-size: 1rem;
-    cursor: pointer;
-    transition: all 0.3s ease;
-  }
-  </style>
