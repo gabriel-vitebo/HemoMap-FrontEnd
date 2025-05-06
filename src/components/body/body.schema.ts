5e37436d50bb61bce7e1fb0bee450f2150d32dd3
gabriel.vitebo <gabriel.vitebo@redealumni.com>
2025-05-06 17:07:02 -0300

    delete all files

--- src/components/body/body.schema.ts
+++
@@ -1,7 +0,0 @@
-import { tv } from "tailwind-variants";
-
-export const schema = tv({
-  slots: {
-    base: 'w-full bg-cyan-950 text-2xl text-white font-bold justify-center flex flex-col',
-  }
-})
