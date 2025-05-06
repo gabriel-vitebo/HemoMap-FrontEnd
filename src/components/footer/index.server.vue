1a2c48caa797b14762b2962cb65c0591040e5ea1
gabriel.vitebo <gabriel.vitebo@redealumni.com>
2025-05-06 17:07:02 -0300

    delete all files

--- src/components/footer/index.server.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="bg-red-800 dark:bg-red-900 text-white p-4 py-4 mt-8">
-    <div class="container mx-auto text-center">
-      <p>&copy; {{ new Date().getFullYear() }} DoaSangue. Todos os direitos reservados.</p>
-    </div>
-  </footer>
-</template>
